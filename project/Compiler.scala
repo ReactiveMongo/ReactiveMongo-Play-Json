62c70eea5a365e2e811f2b42a58d781db4807897
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-07 20:38:03 +0200

    Update play-json to 2.10.5 (#385)

--- project/Compiler.scala
+++ project/Compiler.scala
@@ -3,7 +3,7 @@ import sbt._
 
 object Compiler {
   val playLower = "2.5.0"
-  val playUpper = "2.10.4"
+  val playUpper = "2.10.5"
 
   private val silencerVer = Def.setting[String] {
     "1.7.13"
